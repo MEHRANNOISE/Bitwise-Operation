b9b6dd5c0e2c773b88cceafc18bdefb9e4586d85
Aref mehranzadeh <dev.arefmehranzadeh@gmail.com>
2024-11-05 14:00:28 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Button, Stack, Typography } from '@mui/material'
+import { useState } from 'react'
+
+
+
+
+const App = () => {
+  
+
+  return (
+    <>
+      <Stack padding={3} alignItems={"center"}  height={"100vh"} sx={{backgroundColor:"red"}}>
+        <Typography variant='h3'>Bitwise Operation</Typography>
+        <Stack>
+          
+        </Stack>
+      </Stack>
+    </>
+  )
+}
+
+export default App
